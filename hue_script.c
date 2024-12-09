c0daed9f1db16422502c05c506bc3f7961c22cfa
cotzo <radu@cotzo.me>
2024-12-09 11:51:20 +0200

    added configurable transitiontime, configurable refresh for all lights timer

--- hue_script.c
+++ hue_script.c
@@ -1,17 +1,19 @@
-//       ___ _    _ _ _           _  _          
-//      | _ \ |_ (_) (_)_ __ ___ | || |_  _ ___ 
+//       ___ _    _ _ _           _  _
+//      | _ \ |_ (_) (_)_ __ ___ | || |_  _ ___
 //      |  _/ ' \| | | | '_ (_-< | __ | || / -_)
 //      |_| |_||_|_|_|_| .__/__/ |_||_|\_,_\___|
-//                     |_|                      
-//       PicoC integration for Loxone Minisever 
+//                     |_|
+//       PicoC integration for Loxone Minisever
 // ***************************************************
 //  Please add your configuration in the highlighted
 //  section below. DO NOT CHANGE ANYTHING ELSE!
 // ***************************************************
 
-#define ENABLE_DEBUGGING    (0)
-#define RX_BUFFER_SIZE      (40000)
-#define RX_BLOCK_SIZE       (128)
+#define ENABLE_DEBUGGING (0)
+#define RX_BUFFER_SIZE (40000)
+#define RX_BLOCK_SIZE (128)
+#define TRANSITION_TIME (10)          // Fade duration in multiple of 100ms
+#define ENABLE_REFRESH_ALL_LIGHTS (0) // Refresh all lights approximately every 60s (to keep hue lights in sync with Loxone Miniserver, if they are changed via the Hue app)
 
 enum bulb_t__
 {
@@ -33,38 +35,62 @@ int bulbId[13];
 //  Configure your system here                   START
 // ***************************************************
 char *IP_ADDRESS = "192.168.1.10";
-char *PORT       = "80";
-char *USERNAME   = "yourphilipshueapikey";
+char *PORT = "80";
+char *USERNAME = "yourphilipshueapikey";
 
 // Set type of bulb
-bulbType[0]  = TYPE_SINGLE_TUNABLE;
-bulbType[1]  = TYPE_SINGLE_TUNABLE;
-bulbType[2]  = TYPE_SINGLE_TUNABLE;
-bulbType[3]  = TYPE_NONE;
-bulbType[4]  = TYPE_NONE;
-bulbType[5]  = TYPE_SINGLE_RGB;
-bulbType[6]  = TYPE_SINGLE_RGB;
-bulbType[7]  = TYPE_NONE;
-bulbType[8]  = TYPE_NONE;
-bulbType[9]  = TYPE_NONE;
-bulbType[10] = TYPE_SINGLE_TUNABLE;
-bulbType[11] = TYPE_SINGLE_TUNABLE;
-bulbType[12] = TYPE_SINGLE_RGB;
 
-// Set bulb or group id
+// I1
+bulbType[0] = TYPE_SINGLE_TUNABLE;
 bulbId[0] = 14;
-bulbId[1] = 15;
-bulbId[2] = 16;
-bulbId[3] = 17;
-bulbId[4] = 18;
-bulbId[5] = 19;
-bulbId[6] = 20;
-bulbId[7] = 21;
-bulbId[8] = 22;
-bulbId[9] = 23;
-bulbId[10] = 25;
-bulbId[11] = 26;
-bulbId[12] = 13;
+
+// I2
+bulbType[1] = TYPE_SINGLE_TUNABLE;
+bulbId[1] = 14;
+
+// I3
+bulbType[2] = TYPE_SINGLE_TUNABLE;
+bulbId[2] = 14;
+
+// I4
+bulbType[3] = TYPE_SINGLE_TUNABLE;
+bulbId[3] = 14;
+
+// I5
+bulbType[4] = TYPE_SINGLE_TUNABLE;
+bulbId[4] = 14;
+
+// I6
+bulbType[5] = TYPE_SINGLE_TUNABLE;
+bulbId[5] = 14;
+
+// I7
+bulbType[6] = TYPE_SINGLE_TUNABLE;
+bulbId[6] = 14;
+
+// I8
+bulbType[7] = TYPE_SINGLE_TUNABLE;
+bulbId[7] = 14;
+
+// I9
+bulbType[8] = TYPE_SINGLE_TUNABLE;
+bulbId[8] = 14;
+
+// I10
+bulbType[9] = TYPE_SINGLE_TUNABLE;
+bulbId[9] = 14;
+
+// I11
+bulbType[10] = TYPE_SINGLE_TUNABLE;
+bulbId[10] = 14;
+
+// I12
+bulbType[11] = TYPE_SINGLE_TUNABLE;
+bulbId[11] = 14;
+
+// I13
+bulbType[12] = TYPE_SINGLE_TUNABLE;
+bulbId[12] = 14;
 
 // ***************************************************
 //  Configure your system here                     END
@@ -72,26 +98,23 @@ bulbId[12] = 13;
 
 int IsGroup(int type)
 {
-    int isGroup = 0;
-    switch(type)
+    switch (type)
     {
-        case TYPE_SINGLE_RGB:
-        case TYPE_SINGLE_TUNABLE:
-        case TYPE_SINGLE_DIM:
-        case TYPE_SINGLE_ONOFF:
-            isGroup = 0;
-            break;
+    case TYPE_SINGLE_RGB:
+    case TYPE_SINGLE_TUNABLE:
+    case TYPE_SINGLE_DIM:
+    case TYPE_SINGLE_ONOFF:
+        return 0;
+        break;
 
-        case TYPE_GROUP_RGB:
-        case TYPE_GROUP_TUNABLE:
-        case TYPE_GROUP_DIM:
-        case TYPE_GROUP_ONOFF:
-            isGroup = 1;
-            break;
+    case TYPE_GROUP_RGB:
+    case TYPE_GROUP_TUNABLE:
+    case TYPE_GROUP_DIM:
+    case TYPE_GROUP_ONOFF:
+        return 1;
+        break;
     }
-    return isGroup;
 }
-
 
 void SendCommand(int isGroup, int id, char *command)
 {
@@ -124,7 +147,7 @@ void SendCommand(int isGroup, int id, char *command)
     {
         sprintf(selector, "groups/%d/action", id);
     }
-    
+
     sprintf(txBuffer, "PUT /api/%s/%s HTTP/1.1\r\nHost: %s\r\nConnection: keep-alive\r\nContent-Length: %d\r\n\r\n%s", USERNAME, selector, IP_ADDRESS, strlen(command), command);
     if (ENABLE_DEBUGGING)
     {
@@ -132,7 +155,7 @@ void SendCommand(int isGroup, int id, char *command)
     }
     stream_write(TcpStream, txBuffer, sizeof(txBuffer));
     stream_flush(TcpStream);
-    
+
     // read stream
     if (ENABLE_DEBUGGING)
     {
@@ -142,7 +165,7 @@ void SendCommand(int isGroup, int id, char *command)
             if (nCnt + nBytesReceived > RX_BUFFER_SIZE)
             {
                 nBytesReceived = -1;
-                break; //File is too large
+                break; // File is too large
             }
             else if (nCnt > 0)
             {
@@ -150,12 +173,11 @@ void SendCommand(int isGroup, int id, char *command)
                 nBytesReceived += nCnt;
             }
         } while (nCnt > 0);
-    
+
         printf(strstr(rxBuffer, "[{"));
     }
     stream_close(TcpStream);
 }
-
 
 void SetOnOff(int id, int state, int isGroup)
 {
@@ -163,7 +185,7 @@ void SetOnOff(int id, int state, int isGroup)
 
     if (0 == state)
     {
-        sprintf(command, "{\"on\": false, \"transitiontime\": 10}");
+        sprintf(command, "{\"on\": false, \"transitiontime\": %d}", TRANSITION_TIME);
     }
     else
     {
@@ -171,7 +193,6 @@ void SetOnOff(int id, int state, int isGroup)
     }
     SendCommand(isGroup, id, command);
 }
-
 
 void SetDim(int id, int brightness, int isGroup)
 {
@@ -187,11 +208,10 @@ void SetDim(int id, int brightness, int isGroup)
         // Change range of brightness 1-100 -> 1-255
         brightness = (brightness * 255) / 100;
 
-        sprintf(command, "{\"on\": true, \"bri\": %d, \"transitiontime\": 10}", brightness);
+        sprintf(command, "{\"on\": true, \"bri\": %d, \"transitiontime\": %d}", TRANSITION_TIME);
         SendCommand(isGroup, id, command);
     }
 }
-
 
 void SetTunable(int id, int value, int isGroup)
 {
@@ -199,24 +219,23 @@ void SetTunable(int id, int value, int isGroup)
     int brightness;
     int temperature;
 
-    brightness  = (value - 200000000) / 10000;                  // 0-100
-    temperature = (value - 200000000) - (brightness * 10000);   // Kelvin 2700 - 6500
+    brightness = (value - 200000000) / 10000;                 // 0-100
+    temperature = (value - 200000000) - (brightness * 10000); // Kelvin 2700 - 6500
 
-    brightness  = (brightness * 255) / 100;                     // 0-255
-    temperature = 1000000 / temperature;                        // 154 - 370
+    brightness = (brightness * 255) / 100; // 0-255
+    temperature = 1000000 / temperature;   // 154 - 370
 
     // Check if input value was set to 0 or brightness is set to 0
-    if ((0 == value)||(0 == brightness))
+    if ((0 == value) || (0 == brightness))
     {
         SetOnOff(id, 0, isGroup);
     }
     else
     {
-        sprintf(command, "{\"on\": true, \"bri\": %d, \"ct\": %d, \"transitiontime\": 10}", brightness, temperature);
+        sprintf(command, "{\"on\": true, \"bri\": %d, \"ct\": %d, \"transitiontime\": %d}", brightness, temperature, TRANSITION_TIME);
         SendCommand(isGroup, id, command);
     }
 }
-
 
 void SetColorXYB(int id, float red, float green, float blue, int isGroup)
 {
@@ -286,11 +305,10 @@ void SetColorXYB(int id, float red, float green, float blue, int isGroup)
         cy = Y / (X + Y + Z);
     }
 
-    sprintf(command, "{\"xy\": [%f,%f],\"bri\": %d,\"on\":true, \"transitiontime\": 10}", cx, cy, bri);
+    sprintf(command, "{\"xy\": [%f,%f],\"bri\": %d,\"on\":true, \"transitiontime\": %d}", cx, cy, bri, TRANSITION_TIME);
 
     SendCommand(isGroup, id, command);
 }
-
 
 void SetColorHSB(int id, float red, float green, float blue, int isGroup)
 {
@@ -360,7 +378,6 @@ void SetColorHSB(int id, float red, float green, float blue, int isGroup)
     SendCommand(isGroup, id, command);
 }
 
-
 void SetRGB(int id, int value, int isGroup)
 {
     int red;
@@ -374,53 +391,51 @@ void SetRGB(int id, int value, int isGroup)
         return;
     }
 
-    blue  = value / 1000000;
+    blue = value / 1000000;
     green = (value - (blue * 1000000)) / 1000;
-    red   = value - (blue * 1000000) - (green * 1000);
+    red = value - (blue * 1000000) - (green * 1000);
 
     // Use ether HSB or XYB to set RGB colors (Choose whatever works best for you!)
     SetColorXYB(id, (float)red, (float)green, (float)blue, isGroup);
-    //SetColorHSB(id, (float)red, (float)green, (float)blue, isGroup);
+    // SetColorHSB(id, (float)red, (float)green, (float)blue, isGroup);
 }
-
 
 void UpdateLamp(int i, int value)
 {
     int id = bulbId[i];
     switch (bulbType[i])
     {
-        case TYPE_SINGLE_RGB:
-        case TYPE_GROUP_RGB:
-            if (value < 200000000)
-            {
-                SetRGB(id, value, IsGroup(bulbType[i]));
-            }
-            else
-            {
-                SetTunable(id, value, IsGroup(bulbType[i]));
-            }
-            break;
+    case TYPE_SINGLE_RGB:
+    case TYPE_GROUP_RGB:
+        if (value < 200000000)
+        {
+            SetRGB(id, value, IsGroup(bulbType[i]));
+        }
+        else
+        {
+            SetTunable(id, value, IsGroup(bulbType[i]));
+        }
+        break;
 
-        case TYPE_SINGLE_TUNABLE:
-        case TYPE_GROUP_TUNABLE:
-            if ((200000000 <= value) || (0 == value))
-            {
-                SetTunable(id, value, IsGroup(bulbType[i]));
-            }
-            break;
+    case TYPE_SINGLE_TUNABLE:
+    case TYPE_GROUP_TUNABLE:
+        if ((200000000 <= value) || (0 == value))
+        {
+            SetTunable(id, value, IsGroup(bulbType[i]));
+        }
+        break;
 
-        case TYPE_SINGLE_DIM:
-        case TYPE_GROUP_DIM:
-            SetDim(id, value, IsGroup(bulbType[i]));
-            break;
+    case TYPE_SINGLE_DIM:
+    case TYPE_GROUP_DIM:
+        SetDim(id, value, IsGroup(bulbType[i]));
+        break;
 
-        case TYPE_SINGLE_ONOFF:
-        case TYPE_GROUP_ONOFF:
-            SetOnOff(id, value, IsGroup(bulbType[i]));
-            break;
+    case TYPE_SINGLE_ONOFF:
+    case TYPE_GROUP_ONOFF:
+        SetOnOff(id, value, IsGroup(bulbType[i]));
+        break;
     }
 }
-
 
 // Main application
 int inputsThatChanged;
@@ -453,17 +468,20 @@ while (1)
         }
     }
 
-    refreshCounter = refreshCounter + 1;
-
-    // Refresh all lights approximately every 60s (to keep hue lights in sync with Loxone Miniserver, if they are changed via the Hue app)
-    if (600 < refreshCounter)
+    if (ENABLE_REFRESH_ALL_LIGHTS)
     {
-        refreshCounter = 0;
-        for (i = 0; i < 13; i++)
+        refreshCounter = refreshCounter + 1;
+
+        if (600 < refreshCounter)
         {
-            UpdateLamp(i, (int)getinput(i));
-            sleep(10);
+            refreshCounter = 0;
+            for (i = 0; i < 13; i++)
+            {
+                UpdateLamp(i, (int)getinput(i));
+                sleep(10);
+            }
         }
     }
+
     sleep(100);
 }
